d1191096aa7fe734f7d3b5e12b9f9b0c0ba11b60
Paweł Mendroch <pawelmendroch7@gmail.com>
2020-06-04 13:26:18 +0200

    Add part1a

---
+++ part1/src/index.js
@@ -0,0 +1,45 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const Header = (props) => <h1>{props.course}</h1>;
+
+const Part = (props) => (
+  <p>
+    {props.part} {props.exercise}
+  </p>
+);
+
+const Content = (props) => (
+  <div>
+    <Part part={props.parts[0]} exercise={props.exercises[0]} />
+    <Part part={props.parts[1]} exercise={props.exercises[1]} />
+    <Part part={props.parts[2]} exercise={props.exercises[2]} />
+  </div>
+);
+
+const Total = (props) => <p>Number of exercises {props.exercisesSum}</p>;
+
+const App = () => {
+  const course = "Half Stack application development";
+  const part1 = "Fundamentals of React";
+  const exercises1 = 10;
+  const part2 = "Using props to pass data";
+  const exercises2 = 7;
+  const part3 = "State of a component";
+  const exercises3 = 14;
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content
+        parts={[part1, part2, part3]}
+        exercises={[exercises1, exercises2, exercises3]}
+      />
+      {/* Alternatively pass each part and exercise value separately */}
+      <Total exercisesSum={exercises1 + exercises2 + exercises3} />
+      {/* Alternatively pass each exercise value separately and sum it inside of Total */}
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
