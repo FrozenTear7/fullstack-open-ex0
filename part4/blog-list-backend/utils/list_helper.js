986b1ed0e437291f5818944226c5fb05113d8746
Paweł Mendroch <pawelmendroch7@gmail.com>
2020-06-08 12:04:12 +0200

    Part4a blog list app with tests

---
+++ part4/blog-list-backend/utils/list_helper.js
@@ -0,0 +1,46 @@
+/* eslint-disable no-unused-vars */
+import _ from 'lodash'
+
+const dummy = (blogs) => {
+  return 1
+}
+
+const totalLikes = (blogs) => {
+  if (blogs.length === 0) return 0
+
+  return blogs.reduce((acc, blog) => acc + blog.likes, 0)
+}
+
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) return null
+
+  return blogs.sort((a, b) => b.likes - a.likes)[0]
+}
+
+const mostBlogs = (blogs) => {
+  if (blogs.length === 0) return null
+
+  return _.chain(blogs)
+    .groupBy('author')
+    .map((value, key) => ({
+      author: key,
+      blogs: value.length,
+    }))
+    .sort((a, b) => b.blogs - a.blogs)
+    .value()[0]
+}
+
+const mostLikes = (blogs) => {
+  if (blogs.length === 0) return null
+
+  return _.chain(blogs)
+    .groupBy('author')
+    .map((value, key) => ({
+      author: key,
+      likes: value.reduce((acc, blog) => acc + blog.likes, 0),
+    }))
+    .sort((a, b) => b.blogs - a.blogs)
+    .value()[0]
+}
+
+export { dummy, totalLikes, favoriteBlog, mostBlogs, mostLikes }
